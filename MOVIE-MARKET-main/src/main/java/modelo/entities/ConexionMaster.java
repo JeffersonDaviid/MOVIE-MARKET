d13a18262eb174b6ecb1c71f2783fea8c0921560
JC <jefferson.chileno@epn.edu.ec>
2024-08-13 18:55:47 -0500

    cambios

--- MOVIE-MARKET-main/src/main/java/modelo/entities/ConexionMaster.java
+++ MOVIE-MARKET-main/src/main/java/modelo/entities/ConexionMaster.java
@@ -1,4 +1,5 @@
 package modelo.entities;
+
 import java.sql.*;
 
 /**
@@ -7,17 +8,16 @@ import java.sql.*;
  */
 public class ConexionMaster {
     private final String base = "orcl";
-    private final String user = "master";
-    private final String password = "master";
-    private final String url="jdbc:oracle:thin:@//Alejandro:1521/"+base;
-    private Connection con=null;
-    
-    public Connection getConexionMaster(){
-        try{
-            con=DriverManager.getConnection(this.url, this.user, this.password);
-        }
-        catch(SQLException e){
-            System.err.println(e);            
+    private final String user = "masterproyecto";
+    private final String password = "masterproyecto";
+    private final String url = "jdbc:oracle:thin:@//Tommygo:1521/" + base;
+    private Connection con = null;
+
+    public Connection getConexionMaster() {
+        try {
+            con = DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (SQLException e) {
+            System.err.println(e);
         }
         return con;
     }
