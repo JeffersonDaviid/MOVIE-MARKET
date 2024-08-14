9bb43c750c11444d9fb521a3bb4418dc3df63418
JC <jefferson.chileno@epn.edu.ec>
2024-08-13 19:05:17 -0500

    cambios

--- MOVIE-MARKET-main/src/main/java/modelo/entities/ConexionReplica.java
+++ MOVIE-MARKET-main/src/main/java/modelo/entities/ConexionReplica.java
@@ -1,4 +1,5 @@
 package modelo.entities;
+
 import java.sql.*;
 
 /**
@@ -7,17 +8,16 @@ import java.sql.*;
  */
 public class ConexionReplica {
     private final String base = "orcl";
-    private final String user = "replica";
-    private final String password = "replica";
-    private final String url="jdbc:oracle:thin:@//Alejandro:1521/"+base;
-    private Connection con=null;
-    
-    public Connection getConexionReplica(){
-        try{
-            con=DriverManager.getConnection(this.url, this.user, this.password);
-        }
-        catch(SQLException e){
-            System.err.println(e);            
+    private final String user = "replicaproyecto";
+    private final String password = "replicaproyecto";
+    private final String url = "jdbc:oracle:thin:@//LAPTOP-3608JNJI:1521/" + base;
+    private Connection con = null;
+
+    public Connection getConexionReplica() {
+        try {
+            con = DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (SQLException e) {
+            System.err.println(e);
         }
         return con;
     }
